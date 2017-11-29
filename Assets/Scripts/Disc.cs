66cd82a53b1173d429c845984e8502453293e234
digiholic <digikun@gmail.com>
2017-11-29 07:37:12 -0700

    Initial Commit

---
+++ Assets/Scripts/Disc.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disc : MonoBehaviour {
+    private Rigidbody rb;
+    public ForceMode force;
+    public float launchforce;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            launchforce += 0.1f;
+        }
+	    if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(launchforce,0,0),force);
+            launchforce = 0.0f;
+        }	
+	}
+}
