b8342b23d4f8fd24a6bb42d6a2e911d2dbb17aed
digiholic <digikun@gmail.com>
2017-12-02 12:02:18 -0700

    Added walls and click swipe launches

--- Assets/Scripts/Disc.cs
+++ Assets/Scripts/Disc.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 public class Disc : MonoBehaviour {
     private Rigidbody rb;
     public ForceMode force;
-    public float launchforce;
+    public float divisor;
+
+    private Vector3 LastMousePos;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.Space))
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
         {
-            launchforce += 0.1f;
+            LastMousePos = Input.mousePosition;
         }
-	    if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetMouseButton(0))
         {
-            rb.AddForce(new Vector3(launchforce,0,0),force);
-            launchforce = 0.0f;
-        }	
+            Vector3 lPos = Camera.main.ScreenToWorldPoint(LastMousePos);
+            lPos.y = 0;
+            Vector3 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mPos.y = 0;
+            Debug.DrawLine(lPos, mPos, Color.black);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 diff = Input.mousePosition - LastMousePos;
+            diff.z = diff.y;
+            diff.y = 0;
+            rb.AddForce(diff / divisor, force);
+        }
 	}
 }
